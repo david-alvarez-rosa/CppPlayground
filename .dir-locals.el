343907a5ac8deb3f8e5c951d508e4b2f4feaaa91
David Álvarez Rosa <david@alvarezrosa.com>
2023-12-02 12:10:49 +0100

    Add .dir-locals.el file for Emacs projectile mode

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil . (
+         (projectile-project-configure-cmd . "cmake -DCMAKE_BUILD_TYPE=Debug -B build")
+         (projectile-project-compilation-cmd . "cmake --build build")
+         (projectile-project-test-cmd . "./build/test/unit/cpp_playground_unit_test_runner")
+         (projectile-project-run-cmd . "./build/cpp_playground")
+         (projectile-project-test-prefix . "test_"))))
