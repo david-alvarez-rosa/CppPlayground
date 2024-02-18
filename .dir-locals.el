08d1c8c864d97462a9ab18ed572901f0b68200bc
David Álvarez Rosa <david@alvarezrosa.com>
2024-02-18 21:07:10 +0100

    Integrate playground with Emacs

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil . (
+         (projectile-project-configure-cmd . "cmake -DCMAKE_BUILD_TYPE=Debug -B build")
+         (projectile-project-compilation-cmd . "cmake --build build")
+         (projectile-project-test-cmd . "cmake --build build && ./build/cpp_leetcode")
+         (projectile-project-run-cmd . "./build/cpp_playground")
+         (projectile-project-test-prefix . "test_"))))
