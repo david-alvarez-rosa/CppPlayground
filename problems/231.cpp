472b2f5ebe6d32b280df3705ff654c677126eedb
David Álvarez Rosa <david@alvarezrosa.com>
2024-02-19 10:17:11 +0100

    Add solution for Leetcode 231

---
+++ problems/231.cpp
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+#include <gtest/gtest.h>
+using namespace std;
+
+class Solution {
+public:
+  bool isPowerOfTwo(int n) {
+    if (n == 0) {
+      return false;
+    }
+    if (n == 1) {
+      return true;
+    }
+    if (n % 2 != 0) {
+      return false;
+    }
+    return isPowerOfTwo(n/2);
+  }
+};
+
+TEST(SolutionTest, Test1) {
+  Solution solution;
+  EXPECT_TRUE(solution.isPowerOfTwo(1));
+}
+
+TEST(SolutionTest, Test2) {
+  Solution solution;
+  EXPECT_TRUE(solution.isPowerOfTwo(16));
+}
+
+TEST(SolutionTest, Test3) {
+  Solution solution;
+  EXPECT_FALSE(solution.isPowerOfTwo(3));
+}
