4f9ee3032643f2ba5a52eed42434b649d15c7036
David Álvarez Rosa <david@alvarezrosa.com>
2024-02-23 21:00:26 +0100

    Minor style improvements for run-tests.sh script

--- run-tests.sh
+++ run-tests.sh
@@ -4,11 +4,11 @@ cp main.cpp main.cpp.backup
 
 find . -name '*.cpp' -not -path './build/*' -mindepth 2 | while read -r file_path
 do
-    echo "Running tests for file $file_path"
+    printf "\n-------- Running tests for file $file_path --------\n"
     cp "$file_path" ./main.cpp
     cmake -DCMAKE_BUILD_TYPE=Release -B build
     cmake --build build
-    ./build/cpp_leetcode || exit 1
+    ./build/cpp_leetcode && printf "Test succeeded :)\n\n" || exit 1
 done
 
 mv main.cpp.backup main.cpp
