b884a51e2819f0af4042a0869760f2cf4d0c7ccc
David Álvarez Rosa <david@alvarezrosa.com>
2024-02-24 01:15:25 +0100

    Refactor solution for Leetcode 1 problem

--- problems/1.cpp
+++ problems/1.cpp
@@ -1,42 +1,34 @@
 #include <bits/stdc++.h>
+#include <gmock/gmock.h>
 #include <gtest/gtest.h>
 
 using namespace std;
+using namespace testing;
 
 class Solution {
  public:
   vector<int> twoSum(vector<int>& nums, int target) {
-    for (int i{0}; i < nums.size(); i++) {
-      for (int j{i + 1}; j < nums.size(); j++) {
-        if (nums[i] + nums[j] == target) {
-          return {i, j};
-        }
-      }
-    }
+    for (int i{0}; i < nums.size(); i++)
+      for (int j{i + 1}; j < nums.size(); j++)
+        if (nums[i] + nums[j] == target) return {i, j};
     return {0, 0};
   }
 };
 
 TEST(SolutionTest, Test1) {
   Solution solution;
-  vector<int> input_1{2, 7, 11, 15};
-  auto actual_sol = solution.twoSum(input_1, 9);
-  vector<int> expected_sol{0, 1};
-  EXPECT_EQ(actual_sol, expected_sol);
+  vector<int> nums{2, 7, 11, 15};
+  EXPECT_THAT(solution.twoSum(nums, 9), UnorderedElementsAre(0, 1));
 }
 
 TEST(SolutionTest, Test2) {
   Solution solution;
-  vector<int> input_1{3, 2, 4};
-  auto actual_sol = solution.twoSum(input_1, 6);
-  vector<int> expected_sol{1, 2};
-  EXPECT_EQ(actual_sol, expected_sol);
+  vector<int> nums{3, 2, 4};
+  EXPECT_THAT(solution.twoSum(nums, 6), UnorderedElementsAre(1, 2));
 }
 
 TEST(SolutionTest, Test3) {
   Solution solution;
-  vector<int> input_1{3, 3};
-  auto actual_sol = solution.twoSum(input_1, 6);
-  vector<int> expected_sol{0, 1};
-  EXPECT_EQ(actual_sol, expected_sol);
+  vector<int> nums{3, 3};
+  EXPECT_THAT(solution.twoSum(nums, 9), UnorderedElementsAre(0, 1));
 }
