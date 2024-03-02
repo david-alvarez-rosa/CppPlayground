10224ed6bbe096161d9e454893b98fd6dd59ef79
David Álvarez Rosa <david@alvarezrosa.com>
2024-03-02 12:58:50 +0100

    refactor: run formatter to fix GitHub actions CI/CD

--- problems/977.cpp
+++ problems/977.cpp
@@ -8,11 +8,9 @@ using namespace testing;
 class Solution {
  public:
   vector<int> sortedSquares(vector<int>& nums) {
-
     for (auto& num : nums) num *= num;
     sort(nums.begin(), nums.end());
     return nums;
-
   }
 };
 
