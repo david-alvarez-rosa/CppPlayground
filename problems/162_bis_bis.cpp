1d7670aa71b079cf2e9bb43d1dfbb56d0fae4582
David Álvarez Rosa <david@alvarezrosa.com>
2024-03-09 13:59:29 +0100

    refactor: use `auto` instead of explicit type for Leetcode 162

--- problems/162_bis_bis.cpp
+++ problems/162_bis_bis.cpp
@@ -8,7 +8,7 @@ class Solution {
   int findPeakElement(vector<int>& nums) {
     size_t left{0}, right{nums.size() - 1};
     while (left < right) {
-      int mid = (left + right) >> 1;
+      auto mid = (left + right) >> 1;
       if (nums[mid] > nums[mid + 1])
         right = mid;
       else
