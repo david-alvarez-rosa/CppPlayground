964036ac9851aad95a81b653300656520bd58954
David Álvarez Rosa <david@alvarezrosa.com>
2024-03-24 21:37:12 +0100

    Add solution for Leetcode 287 problem

---
+++ problems/287.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <gtest/gtest.h>
+
+using namespace std;
+
+class Solution {
+ public:
+  int findDuplicate(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    for (size_t i{1}; i < nums.size(); i++)
+      if (nums[i] == nums[i - 1]) return nums[i];
+    return -1;
+  }
+};
+TEST(SolutionTest, Test1) {
+  Solution solution;
+  vector<int> nums{1, 3, 4, 2, 2};
+  EXPECT_EQ(2, solution.findDuplicate(nums));
+}
+
+TEST(SolutionTest, Test2) {
+  Solution solution;
+  vector<int> nums{3, 1, 3, 4, 2};
+  EXPECT_EQ(3, solution.findDuplicate(nums));
+}
+
+TEST(SolutionTest, Test3) {
+  Solution solution;
+  vector<int> nums{3, 3, 3, 3, 3};
+  EXPECT_EQ(3, solution.findDuplicate(nums));
+}
