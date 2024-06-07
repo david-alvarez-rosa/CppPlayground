fb0f20d3df1901a296a31cd94f843bc067778152
David Álvarez Rosa <david@alvarezrosa.com>
2024-06-07 18:15:26 +0200

    Add helpful comments

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,7 @@
 ((nil . (
          (projectile-project-configure-cmd . "cmake -DCMAKE_BUILD_TYPE=Debug -B build")
          (projectile-project-compilation-cmd . "cmake --build build")
+         ;; useful to use --gtest_filter=MySuite.MyTest
          (projectile-project-test-cmd . "cmake --build build && ./build/cpp_leetcode")
          (projectile-project-run-cmd . "./build/cpp_leetcode")
          (projectile-project-test-prefix . "test_"))))
