5788e32684c6727d822cf43ce58fd97466594440
David Álvarez Rosa <david@alvarezrosa.com>
2024-11-02 17:17:31 +0000

    Fix .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,6 +2,6 @@
          (projectile-project-configure-cmd . "cmake -DCMAKE_BUILD_TYPE=Debug -B build")
          (projectile-project-compilation-cmd . "cmake --build build")
          ;; useful to use --gtest_filter=MySuite.MyTest
-         (projectile-project-test-cmd . "cmake --build build && ./build/cpp_leetcode")
-         (projectile-project-run-cmd . "./build/cpp_leetcode")
+         (projectile-project-test-cmd . "cmake --build build && ./build/cpp_playground")
+         (projectile-project-run-cmd . "./build/cpp_playground")
          (projectile-project-test-prefix . "test_"))))
