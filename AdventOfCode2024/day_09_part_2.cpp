28e71d94358bfb172d51b41e005b39db33670f2d
David Álvarez Rosa <david@alvarezrosa.com>
2024-12-09 12:46:09 +0000

    [AoC] Add solution for day 9 part 2

---
+++ AdventOfCode2024/day_09_part_2.cpp
@@ -0,0 +1,100 @@
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using DiskMap = std::vector<uint16_t>;
+using Disk = std::vector<int16_t>;
+
+namespace {
+constexpr const auto kFreeBlockId{-1};
+}  // namespace
+
+auto ComputeChecksum(const Disk& disk) -> long long {
+  // Pre-req disk is compacted
+  auto checksum{0LL};
+  auto i{-1};
+  while (++i < disk.size()) {
+    if (disk[i] != kFreeBlockId) {
+      checksum += i * disk[i];
+    }
+  }
+  return checksum;
+}
+
+auto CompactDisk(Disk& disk) {
+  int right = disk.size() - 1;
+  while (right >= 0) {
+    // Next block
+    while (right >= 0 && disk[right] == kFreeBlockId) {
+      --right;
+    }
+    auto block_id = disk[right];
+    auto block_size{0};
+    while (right >= 0 && disk[right] == block_id) {
+      ++block_size;
+      --right;
+    }
+
+    // Free block
+    auto free_block_found{false};
+    auto left{0};
+    while (!free_block_found && left < right) {
+      if (disk[left] != kFreeBlockId) {
+        ++left;
+        continue;
+      }
+      auto free_block_size{0};
+      while (disk[left] == kFreeBlockId && free_block_size < block_size) {
+        ++left;
+        ++free_block_size;
+      };
+
+      if (free_block_size == block_size) {
+        free_block_found = true;
+      }
+    }
+    if (free_block_found) {
+      while (block_size--) {
+        disk[right + 1 + block_size] = kFreeBlockId;
+        disk[left - 1 - block_size] = block_id;
+      }
+    }
+  }
+}
+
+auto ConvertToDisk(const DiskMap& disk_map) -> Disk {
+  auto disk = Disk{};
+  for (auto i{0U}, id{0U}; i < disk_map.size(); i += 2, ++id) {
+    auto file_blocks = disk_map[i];
+    auto free_blocks = disk_map[i + 1];
+    while (file_blocks--) {
+      disk.push_back(id);
+    }
+    while (free_blocks--) {
+      disk.push_back(kFreeBlockId);
+    }
+  }
+  return disk;
+};
+
+auto ParseInputFile(const std::string& file_name) -> DiskMap {
+  auto disk_map = DiskMap{};
+  auto file_stream = std::ifstream{file_name};
+  auto digits_raw = std::string{};
+  file_stream >> digits_raw;
+
+  for (auto digit_raw : digits_raw) {
+    disk_map.push_back(digit_raw - '0');
+  }
+
+  return disk_map;
+}
+
+auto main(int argc, char* argv[]) -> int {
+  auto disk_map = ParseInputFile(argv[1]);
+  auto disk = ConvertToDisk(disk_map);
+  CompactDisk(disk);
+  std::cout << ComputeChecksum(disk) << "\n";
+}
