439aec687355903a2845ad08df4bf83105183ba1
David Álvarez Rosa <david@alvarezrosa.com>
2025-03-19 12:26:55 +0000

    Switch from Emacs projectile to built-in project.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,1 @@
-((nil . (
-         (projectile-project-configure-cmd . "cmake -DCMAKE_BUILD_TYPE=Debug -B build")
-         (projectile-project-compilation-cmd . "cmake --build build")
-         ;; useful to use --gtest_filter=MySuite.MyTest
-         (projectile-project-test-cmd . "cmake --build build && ./build/cpp_playground")
-         (projectile-project-run-cmd . "./build/cpp_playground")
-         (projectile-project-test-prefix . "test_"))))
+((nil . ((compile-command . "cmake --build build && build/cpp_playground"))))
