23b40798759165b46a0b653f08d315d400490ed4
David Álvarez Rosa <david@alvarezrosa.com>
2025-08-30 00:18:01 +0100

    Improve CppPlayground enviroment
    Even more generic, rename Leetcode to test, and improve README.org

--- run-tests.sh
+++ run-tests.sh
@@ -12,9 +12,9 @@ git diff --name-only HEAD~9 HEAD | grep -E 'Leetcode/.*cpp' | while read -r file
 do
     [ -f $file_path ] || continue
     printf "\n-------- Running tests for file %s --------\n" "$file_path"
-    cp "$file_path" ./leetcode.cpp
+    cp "$file_path" ./test.cpp
     cmake --build build
-    ./build/leetcode && printf "Test succeeded :)\n\n" || exit 1
+    ./build/test && printf "Test succeeded :)\n\n" || exit 1
 done
 
 mv main.cpp.backup main.cpp
