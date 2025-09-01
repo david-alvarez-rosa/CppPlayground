f658009a85051f73c098dc72b8b6ef1d78244a00
David Álvarez Rosa <david@alvarezrosa.com>
2025-09-01 17:06:57 +0100

    Update run-leetcode-tests.sh pipeline script

--- .github/run-leetcode-tests.sh
+++ .github/run-leetcode-tests.sh
@@ -8,11 +8,11 @@ export CC=clang
 export CXX=clang++
 cmake -S . -B build -G Ninja
 
-git diff --name-only HEAD~9 HEAD | grep -E 'Leetcode/.*cpp' | while read -r file_path
+git diff --name-only HEAD~9 HEAD | grep -E "Leetcode/.*cpp" | while read -r file_path
 do
     [ -f $file_path ] || continue
     printf "\n-------- Running tests for file %s --------\n" "$file_path"
-    cp "$file_path" ./test.cpp
+    cp $file_path test.cpp
     cmake --build build
     ./build/test && printf "Test succeeded :)\n\n" || exit 1
 done
