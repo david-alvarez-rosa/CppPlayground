b9f6bc41c6a4dec3b2ecfaba11ccf27bd591a5c9
David Álvarez Rosa <david@alvarezrosa.com>
2026-06-24 17:45:39 +0100

    Fixes for rename test.cpp to tests.cpp for consistency

--- scripts/run-leetcode-tests.sh
+++ scripts/run-leetcode-tests.sh
@@ -6,7 +6,7 @@ export ASAN_OPTIONS=detect_leaks=0
 
 profile="${CONAN_PROFILE:-conan/profile}"
 
-cp scratch/test.cpp scratch/test.cpp.backup
+cp scratch/tests.cpp scratch/tests.cpp.backup
 
 [ -f build/debug/conan_toolchain.cmake ] || conan install . -of build/debug -pr "$profile" --build=missing --lockfile=conan.lock -s build_type=Debug
 cmake --preset debug
@@ -15,9 +15,9 @@ git diff --name-only --diff-filter=AM HEAD~9 HEAD | grep -E "leetcode/.*cpp" | w
 do
     [ -f "$file_path" ] || continue
     printf "\n-------- Running tests for file %s --------\n" "$file_path"
-    cp "$file_path" scratch/test.cpp
+    cp "$file_path" scratch/tests.cpp
     cmake --build --preset debug --target tests
     ctest --preset debug --output-on-failure && printf "Test succeeded :)\n\n" || exit 1
 done
 
-mv scratch/test.cpp.backup scratch/test.cpp
+mv scratch/tests.cpp.backup scratch/tests.cpp
